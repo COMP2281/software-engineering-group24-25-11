0c5d5ce28b34d2d43c2b1a6c6aeade9ce23a0dcf
Keane <keanewongjl@gmail.com>
2025-02-12 10:53:13 +0000

    quiz struct setup

--- quiz_logic/src/main.rs
+++ quiz_logic/src/main.rs
@@ -1,23 +1,42 @@
+
 use std::error::Error;
 use std::fs::File;
 use csv::ReaderBuilder;
+use serde_derive::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Question {
+    question: String,
+    choice1: String,
+    choice2: String,
+    choice3: String,
+    choice4: String,
+    answer: String,
+    #[serde(deserialize_with = "csv::invalid_option")]
+    difficulty: Option<f64>,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Open the CSV file
+    // open the CSV file
     let file_path = "question_bank/ai_modules.csv";
     let file = File::open(file_path)?;
 
-    // Create a CSV reader
+    // create a CSV reader
     let mut rdr = ReaderBuilder::new()
-        .has_headers(true) // Set to true if your CSV has headers
+        .has_headers(true) 
         .from_reader(file);
 
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
+    let mut questions = Vec::new();
+
+    for result in rdr.deserialize() {
+        let record: Question = result?;
+        questions.push(record);
     }
 
-    
+    println!("{:?}", questions);
+
+
 
     Ok(())
 }
